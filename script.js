034d8bbb1f42f9c828978bcf24e8e6b4c33076ba
Abduvakhob Abdurazzakov <abduvakhobabdurazzakov@MacBook-Pro.local>
2022-03-19 10:54:44 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// "use strict";
+// console.log("hello ");
+// alert("Hello world");
+// confirm("are you over 18+ ?")
+
+// const array = ["red", "black", "yellow"];
+// console.log(array[2]);
+
+// const userAge = confirm("are you over 18+ ?");
+// console.log(userAge);
+
+// prompt("ismiz nma ?");
+// const answers = [];
+// answers[0] = prompt("what is your name ?"); ßß
+// console.log(answers);
+
+// // const firstName = prompt("isiz nma ?");
+// console.log(firstName);
+
+// const firstName = "samar";
+// console.log(`mani ismim ${firstName}`);
+
+// const xName = prompt("enter your name", "");
+// console.log(`User's name ${xName}`);
+
+// console.log(5 + 5);
+// console.log(5 - 5);
+// console.log(5 * 5);
+// console.log(5 / 5);
+
+// let z = 10;
+// console.log(++z)
+// console.log(z++);
+// const ischecked = true;
+// const isClose = true;
+// console.log(ischecked && isClose);
